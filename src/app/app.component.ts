7c75d596c45d791e4425f17ccb7fe5c00a1d0efb
UsmanFarooq77 <usmanfarooq7721@gmail.com>
2021-10-19 01:06:02 +0500

    Fetch remote data from web api.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit, ViewChild } from '@angular/core';
+// import { sampleData } from './jsontreegriddata';
+import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
+import {  EditSettingsModel } from '@syncfusion/ej2-treegrid';
+import { TreeGridComponent,ColumnChooserService, ToolbarService } from '@syncfusion/ej2-angular-treegrid';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    
 })
-export class AppComponent {
-  title = 'ej2-syncfusion';
+export class AppComponent implements OnInit {
+  public data: DataManager;
+  public pageSetting: Object;
+
+  @ViewChild('treegrid')
+  public treegrid: TreeGridComponent;
+
+    // public data: Object[] = [];
+    public pageSettings: Object;
+    public contextMenuItems: string[] = [];
+    public editing: EditSettingsModel;
+    public toolbar: string[];
+    public editparams: Object;
+
+    ngOnInit(): void {
+      this.data =  new DataManager({ url: 'https://ej2services.syncfusion.com/production/web-services/api/SelfReferenceData', adaptor: new WebApiAdaptor });
+      this.pageSetting = { pageCount: 3 };
+        // this.data = sampleData;
+        this.contextMenuItems = ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending',
+         'Edit', 'Delete', 'Save', 'Cancel',
+        'PdfExport', 'ExcelExport', 'CsvExport', 'FirstPage', 'PrevPage',
+        'LastPage', 'NextPage'];
+    this.editing = { allowDeleting: true, allowEditing: true, mode: 'Row' };
+    // this.pageSettings= { pageSize: 10 };
+    this.editparams = {params: { format: 'n' }};
+    this.toolbar = ['ColumnChooser'];
+
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { TreeGridModule } from '@syncfusion/ej2-angular-treegrid';
+import { SortService, ResizeService, PageService, EditService, ExcelExportService, PdfExportService, ContextMenuService } from '@syncfusion/ej2-angular-treegrid';
+import { ColumnChooserService, ToolbarService } from '@syncfusion/ej2-angular-treegrid';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    TreeGridModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [SortService, ResizeService, PageService, EditService, ExcelExportService, PdfExportService, ContextMenuService
+    ,ColumnChooserService, ToolbarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
